085a38aca0726db03bb4a8d5a3ac2431d1961e61
Khueminh2025 <khueminh2025@gmail.com>
2025-07-08 22:22:48 +0700

    Thêm lệnh tạo superuser vào build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 # Build script for Render
+
+# Cài các thư viện
 pip install -r requirements.txt
+
+# Thu thập static files
 python manage.py collectstatic --no-input
+
+# Di chuyển dữ liệu
 python manage.py migrate
+
+# Tạo superuser (chỉ chạy nếu chưa tồn tại)
+python manage.py createsuperuser \
+  --noinput \
+  --username $DJANGO_SUPERUSER_USERNAME \
+  --email $DJANGO_SUPERUSER_EMAIL || true
